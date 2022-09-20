6dfdcebbb903fa3f1c5e3e635187ca66dac3205f
tiannian <dtiannian@gmail.com>
2022-09-20 16:30:04 +0800

    remove check sum

--- build.rs
+++ build.rs
@@ -1,17 +1,37 @@
-use std::{collections::HashMap, env, fs, path::Path};
+use std::{collections::HashMap, env, fs, io::Read, path::Path, process::Command};
 
 use flate2::read::GzDecoder;
-use sha2::{Digest, Sha256};
+use sha2::{digest::Output, Digest, Sha256};
 use tar::Archive;
 
+pub struct DigestReader<R, D> {
+    reader: R,
+    digester: D,
+}
+
+impl<R: Read, D: Digest + Clone> DigestReader<R, D> {
+    pub fn new(reader: R, digester: D) -> Self {
+        Self { reader, digester }
+    }
+
+    pub fn digest(&mut self) -> Output<D> {
+        self.digester.clone().finalize()
+    }
+}
+
+impl<R: Read, D: Digest> Read for DigestReader<R, D> {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        let len = self.reader.read(buf)?;
+
+        println!("{}", len);
+
+        self.digester.update(buf);
+
+        Ok(len)
+    }
+}
+
 fn check_and_download(td_name: &str, version: &str) {
-    let filename = format!("tendermint_{}_{}.tar.gz", version, td_name);
-
-    let url = format!(
-        "https://github.com/tendermint/tendermint/releases/download/v0.34.21/{}",
-        filename
-    );
-
     let out_dir = env::var("OUT_DIR").unwrap();
 
     let out_file = format!("{}/tendermint", out_dir);
@@ -19,75 +39,68 @@ fn check_and_download(td_name: &str, version: &str) {
     let out_path = Path::new(&out_file);
 
     if out_path.exists() {
-        if check(&out_file, &filename) {
+        let output = Command::new(out_path).arg("version").output().unwrap();
+        if output.status.success() {
             return;
+        } else {
+            fs::remove_file(out_path).unwrap();
         }
-    } else {
-        download(&url, &out_file);
-
-        assert!(check(&out_file, &filename), "Checksum failed.");
     }
+    download(td_name, version, &out_file);
 }
 
-fn build_hash_map() -> HashMap<String, String> {
-    let mut hm = HashMap::new();
+/* fn build_hash_map() -> HashMap<String, String> { */
+/* let mut hm = HashMap::new(); */
+/*  */
+/* hm.insert( */
+/*     String::from("tendermint_0.34.21_darwin_arm64.tar.gz"), */
+/*     String::from("4a37152f5bd93544ed485124b6f8c9684d80adba5096f6675aab7c2b01af6c64"), */
+/* ); */
+/* hm.insert( */
+/*     String::from("tendermint_0.34.21_windows_amd64.tar.gz"), */
+/*     String::from("587bb0f47e2721be9c202113597c6b2188e97fa1e36299d2ed821700f40c195a"), */
+/* ); */
+/* hm.insert( */
+/*     String::from("tendermint_0.34.21_windows_arm64.tar.gz"), */
+/*     String::from("593ab9cf5a2c45dfcec9061f52028f882e0761c85d5dac6a4e04c2d23f440902"), */
+/* ); */
+/* hm.insert( */
+/*     String::from("tendermint_0.34.21_windows_armv6.tar.gz"), */
+/*     String::from("936a9eeb74fd0029f29a5995bc3cd0252773b70036cba07304de3e74cb3970bc"), */
+/* ); */
+/* hm.insert( */
+/*     String::from("tendermint_0.34.21_linux_arm64.tar.gz"), */
+/*     String::from("9595eaa7743974bfa64912d0abedcf1fcd459ff16c73b4c2faa52e9f6f426378"), */
+/* ); */
+/* hm.insert( */
+/*     String::from("tendermint_0.34.21_linux_amd64.tar.gz"), */
+/*     String::from("c260c2db3e6faac711b767b095bb7c3c1afe51b624f97bfb675aa8063d1530c7"), */
+/* ); */
+/* hm.insert( */
+/*     String::from("tendermint_0.34.21_linux_armv6.tar.gz"), */
+/*     String::from("c6d77a8c5066bf3b337fa956ab038fec358bfb307bd6f5cdc11b7bda9642436e"), */
+/* ); */
+/* hm.insert( */
+/*     String::from("tendermint_0.34.21_darwin_amd64.tar.gz"), */
+/*     String::from("e2648f5bfc7ef173bd15114e40ebf9350ab64f3931faedb296501ba14c9143fc"), */
+/* ); */
+/*  */
+/* hm */
+/* } */
 
-    hm.insert(
-        String::from("tendermint_0.34.21_darwin_arm64.tar.gz"),
-        String::from("4a37152f5bd93544ed485124b6f8c9684d80adba5096f6675aab7c2b01af6c64"),
-    );
-    hm.insert(
-        String::from("tendermint_0.34.21_windows_amd64.tar.gz"),
-        String::from("587bb0f47e2721be9c202113597c6b2188e97fa1e36299d2ed821700f40c195a"),
-    );
-    hm.insert(
-        String::from("tendermint_0.34.21_windows_arm64.tar.gz"),
-        String::from("593ab9cf5a2c45dfcec9061f52028f882e0761c85d5dac6a4e04c2d23f440902"),
-    );
-    hm.insert(
-        String::from("tendermint_0.34.21_windows_armv6.tar.gz"),
-        String::from("936a9eeb74fd0029f29a5995bc3cd0252773b70036cba07304de3e74cb3970bc"),
-    );
-    hm.insert(
-        String::from("tendermint_0.34.21_linux_arm64.tar.gz"),
-        String::from("9595eaa7743974bfa64912d0abedcf1fcd459ff16c73b4c2faa52e9f6f426378"),
-    );
-    hm.insert(
-        String::from("tendermint_0.34.21_linux_amd64.tar.gz"),
-        String::from("c260c2db3e6faac711b767b095bb7c3c1afe51b624f97bfb675aa8063d1530c7"),
-    );
-    hm.insert(
-        String::from("tendermint_0.34.21_linux_armv6.tar.gz"),
-        String::from("c6d77a8c5066bf3b337fa956ab038fec358bfb307bd6f5cdc11b7bda9642436e"),
-    );
-    hm.insert(
-        String::from("tendermint_0.34.21_darwin_amd64.tar.gz"),
-        String::from("e2648f5bfc7ef173bd15114e40ebf9350ab64f3931faedb296501ba14c9143fc"),
+fn download(platform: &str, version: &str, target: &str) {
+    let filename = format!("tendermint_{}_{}.tar.gz", version, platform);
+
+    let url = format!(
+        "https://github.com/tendermint/tendermint/releases/download/v{}/{}",
+        version, filename
     );
 
-    hm
-}
-
-fn check(out_file: &str, filename: &str) -> bool {
-    let map = build_hash_map();
-
-    if let Some(r) = map.get(filename) {
-        let bytes = fs::read(out_file).unwrap();
-
-        let digest = Sha256::digest(bytes);
-
-        let hex_digest = hex::encode(digest);
-
-        &hex_digest == r
-    } else {
-        false
-    }
-}
-
-fn download(url: &str, target: &str) {
     let body = reqwest::blocking::get(url).unwrap();
 
-    let decoder = GzDecoder::new(body);
+    let digester = DigestReader::new(body, Sha256::new());
+
+    let decoder = GzDecoder::new(digester);
 
     let mut archive = Archive::new(decoder);
 
@@ -101,6 +114,12 @@ fn download(url: &str, target: &str) {
             entry.unpack(target).unwrap();
         }
     }
+
+    /*     let hash_bytes = archive.into_inner().into_inner().digest(); */
+    /*  */
+    /* let map = build_hash_map(); */
+    /*  */
+    /* assert_eq!(Some(&hex::encode(hash_bytes)), map.get(&filename)); */
 }
 
 fn main() {
